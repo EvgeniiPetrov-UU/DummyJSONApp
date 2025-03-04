3b5a5dc583431ebb413b6d586980a83a75ee1908
Evgenii <YOKoSo.anon@yandex.ru>
2025-03-05 01:01:52 +0300

    Initial commit

---
+++ src/test/java/com/example/dummyjsonapp/DummyJsonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummyjsonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummyJsonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
